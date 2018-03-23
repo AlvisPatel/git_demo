f3514a784f9b51e3fd079c697c9e59e7c12fc56b
AlvisPatel <vadaliyaalvis@gmail.com>
2018-03-23 11:16:12 +0530

    second commit i guess

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+	//$con=mysql_connect("localhost","root","") or die("Connection is not connect, please try aging");
+	//$selectDb = mysql_select_db("bank") or die("Database is not select");
+	@session_start();
+	$con=mysqli_connect("localhost","root","","myquestions") or die("Connection is not connect, please try aging");
+?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
---
+++ demo1.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    	<link href="user/assete/css/bootstrap.min.css" rel="stylesheet">
+    	<link href="user/assete/css/coustm.css" rel="stylesheet" type="text/css">
+    	<link rel="stylesheet" href="user/assete/css/coustm.css" rel="stylesheet"
+    	<link rel="stylesheet" type="text/css" href="../webfonts/fontawesome-all.min.css">
+    	<script src="user/assete/js/jquery.min.js"></script>
+    	<style type="text/css">
+    		/**{
+    			border:1px solid black;
+    		}*/
+    	</style>
+</head>
+
+<body>
+	<div class="container-fluid">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3 col-md-6 col-sm-12">
+				<div style="margin: 2px; float:left;">
+					<img src="img/user/user_img.png" style="height:60px; width:60px;">
+				</div>
+				<div style="margin-left:5px; float:left; margin-top: 2px;">
+					<div style="font-size: 16px;">
+						Shiyani venish
+					</div>
+					<div style="font-size: 12px;">
+						Gujarat,india
+					</div>
+					<div style="font-size: 12px;">
+						<b>1200</b>
+					</div>
+				</div>
+				<div style="clear: both;"></div>
+				<div style="margin-left: 67px; margin-top:-2px; font-size: 14px;">
+					<a >c,</a>
+					<a>c++,</a>
+					<a>java</a>
+					</div>
+			</div>
+			<div class="col-lg-3 col-md-6 col-sm-12">
+				<div style="margin: 2px; float:left;">
+					<img src="img/user/user_img.png" style="height:60px; width:60px;">
+				</div>
+				<div style="margin-left:5px; float:left; margin-top: 2px;">
+					<div style="font-size: 16px;">
+						Shiyani venish
+					</div>
+					<div style="font-size: 12px;">
+						Gujarat,india
+					</div>
+					<div style="font-size: 12px;">
+						<b>1200</b>
+					</div>
+				</div>
+				<div style="clear: both;"></div>
+				<div style="margin-left: 67px; margin-top:-2px; font-size: 14px;">
+					<a >c,</a>
+					<a>c++,</a>
+					<a>java</a>
+					</div>
+			</div><div class="col-lg-3 col-md-6 col-sm-12">
+				<div style="margin: 2px; float:left;">
+					<img src="img/user/user_img.png" style="height:60px; width:60px;">
+				</div>
+				<div style="margin-left:5px; float:left; margin-top: 2px;">
+					<div style="font-size: 16px;">
+						Shiyani venish
+					</div>
+					<div style="font-size: 12px;">
+						Gujarat,india
+					</div>
+					<div style="font-size: 12px;">
+						<b>1200</b>
+					</div>
+				</div>
+				<div style="clear: both;"></div>
+				<div style="margin-left: 67px; margin-top:-2px; font-size: 14px;">
+					<a >c,</a>
+					<a>c++,</a>
+					<a>java</a>
+					</div>
+			</div><div class="col-lg-3 col-md-6 col-sm-12">
+				<div style="margin: 2px; float:left;">
+					<img src="img/user/user_img.png" style="height:60px; width:60px;">
+				</div>
+				<div style="margin-left:5px; float:left; margin-top: 2px;">
+					<div style="font-size: 16px;">
+						Shiyani venish
+					</div>
+					<div style="font-size: 12px;">
+						Gujarat,india
+					</div>
+					<div style="font-size: 12px;">
+						<b>1200</b>
+					</div>
+				</div>
+				<div style="clear: both;"></div>
+				<div style="margin-left: 67px; margin-top:-2px; font-size: 14px;"> 
+					<a >c,</a> 
+					<a>c++,</a> 
+					<a>java</a>
+					</div>
+			</div>
+			
+		</div>
+
+		</div>
+	</div>
+</body>
+</html>
